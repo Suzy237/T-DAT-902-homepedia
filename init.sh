8e78f16c17e117be40bee40ac62de88971fc4b13
Suzy237 <81296449+Suzy237@users.noreply.github.com>
2024-05-25 15:56:32 +0200

    Modification du docker-compose

--- init.sh
+++ init.sh
@@ -2,13 +2,6 @@
 set -e
 
 mongosh <<EOF
-use homepedia
-db.createUser({
-  user: '$MONGODB_USER',
-  pwd:  '$MONGODB_PASSWORD',
-  roles: [{
-    role: 'readWrite',
-    db: '$MONGODB_DB'
-  }]
-})
+use ${MONGO_INITDB_DATABASE}
+
 EOF

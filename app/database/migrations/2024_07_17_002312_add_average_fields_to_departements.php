e61b07b4949c5e287508873c4fde11f8bd4337e7
icarofr <icaro@gmx.fr>
2024-07-17 00:24:01 +0200

    feat: added mongodb to composer

---
+++ app/database/migrations/2024_07_17_002312_add_average_fields_to_departements.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('departements', function (Blueprint $table) {
+            $table->decimal('average_cost', 15, 2)->nullable();
+            $table->decimal('safety_rate', 20, 10)->nullable();
+            $table->decimal('valeur_fonciere', 15, 2)->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('departements', function (Blueprint $table) {
+            //
+        });
+    }
+};
